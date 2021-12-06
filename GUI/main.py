b22805f82f1d603063c58dd62d25773286cbb339
Patryk  Staszczuk <patrykstaszczuk@MacBook-Pro-Patryk.local>
2021-12-06 12:55:31 +0100

    create new GUI

--- GUI/main.py
+++ GUI/main.py
@@ -1,112 +1,166 @@
-import tkinter as tk
+from tkinter import (
+    Tk,
+    Label,
+    Button,
+    Frame,
+    IntVar,
+    StringVar,
+    OptionMenu,
+    Entry
+    )
+from PIL import Image, ImageTk
+
 from pattern_generator import ImageBackground, SimplePolishSchema, Pattern
-import os
-import sys
-import subprocess
 
 
-class App(tk.Frame):
-    def __init__(self, root):
-        super().__init__(root)
-        root.bind('<Return>', self.callback)
-        self.grid()
+class BackgroundSettings:
+    def __init__(self, master):
+        self.frame = Frame(master, width=550, height=350)
+        self.frame.grid(row=0, column=0, padx=25, pady=5)
+        self.frame.grid_propagate(0)
+        self.frame['relief'] = 'raised'
+        self.frame['borderwidth'] = 1
 
-        self.custom_settings_frame = self.generate_custom_settings_frame()
-        self.custom_pattern_settings_frame = self.generate_pattern_settings_frame()
+        self.error_msg = Label(self.frame)
 
-        self.error_msg = tk.Label(self, text=f'', bg='red')
-        self.succes_msg = tk.Label(self, text='', bg='green')
+        Label(self.frame,
+              text='How width (in pixsels) you want the image be? Max(5616)? ').grid(row=0, column=0, sticky='w', pady=10)
+        self.width_input = IntVar()
+        self.width_input.set(4000)
+        self.width = Entry(self.frame, textvariable=self.width_input, width=15)
+        self.width.grid(row=0, column=1, sticky='e')
 
-        self.background = None
-        self.pattern = None
-        self.schema = None
+        Label(self.frame,
+              text='How many letters in row you want?: ').grid(row=1, column=0, sticky='w', pady=10)
+        self.num_of_columns_input = IntVar()
+        self.num_of_columns_input.set(7)
+        self.num_of_columns = Entry(
+            self.frame, textvariable=self.num_of_columns_input, width=15)
+        self.num_of_columns.grid(row=1, column=1, sticky='e')
 
-    def callback(self, event) -> None:
-        """ if Enter key is clicked in pattern settings,
-         pass all current value to create_pattern method """
-        if event.widget.master == self.custom_pattern_settings_frame:
-            kwargs = {}
-            for child in self.custom_pattern_settings_frame.winfo_children():
-                if child.widgetName == 'entry':
-                    kwargs[child.name] = child.get()
-            self.create_pattern(**kwargs)
+        Label(self.frame,
+              text='Do you want to genreate background mesh?:(yes/no) ').grid(row=2, column=0, sticky='w', pady=10)
+        self.with_mesh_input = StringVar()
+        self.with_mesh_input.set('yes')
+        self.with_mesh = Entry(self.frame,
+                               textvariable=self.with_mesh_input, width=15)
+        self.with_mesh.grid(row=2, column=1, sticky='e')
 
-    def generate_pattern_settings_frame(self) -> tk.Frame:
-        pattern_frame = tk.Frame(self)
-        tk.Label(pattern_frame,
-                 text='Please input desire line width. Type 0 to use default: ').grid(row=0, column=0)
-        width_input = tk.IntVar()
+        Label(self.frame,
+              text='Input desire background color in hex format (#c8fd92). ').grid(row=3, column=0, sticky='w', pady=10)
+        self.color_input = StringVar()
+        self.color_input.set('#FFFFFF')
+        self.color = Entry(self.frame, textvariable=self.color_input, width=15)
+        self.color.grid(row=3, column=1, sticky='e')
+        Label(self.frame,
+              text='Select schema ').grid(row=4, column=0, sticky='w', pady=10)
+        self.schema_input = StringVar()
+        self.schema_input.set('SimplePolishSchema')
+        self.schema = OptionMenu(
+            self.frame, variable=self.schema_input, value='SimplePolishSchema')
+        self.schema.grid(row=4, column=1, sticky='e')
+        Label(self.frame, pady=20).grid(row=5)
+        self.apply_button = Button(
+            self.frame,
+            pady=10,
+            padx=10,
+            text='Apply',
+        )
+        self.apply_button.grid(row=6, column=0)
+
+
+class BackgroundImage:
+    def __init__(self, master):
+        self.frame = Frame(master, width=390, height=370)
+        self.frame.grid(row=0, column=1, pady=5)
+
+
+class PatternSettings:
+    def __init__(self, master):
+        self.frame = Frame(master, width=550, height=350)
+        self.frame.grid(row=1, column=0, padx=25, pady=5)
+        self.frame.grid_propagate(0)
+        self.frame['relief'] = 'raised'
+        self.frame['borderwidth'] = 1
+
+        self.msg = Label(self.frame)
+
+        Label(self.frame,
+              text='Please input desire line width. Type 0 to use default: ').grid(row=0, column=0, pady=10)
+        width_input = IntVar()
         width_input.set(0)
-        width = tk.Entry(pattern_frame, textvariable=width_input)
-        width.grid(row=0, column=1)
-        width.name = 'width'
-        tk.Label(pattern_frame,
-                 text='Input desire line color in hex format (#c8fd92): ').grid(row=1, column=0)
-        color_input = tk.StringVar()
+        self.width = Entry(self.frame, textvariable=width_input)
+        self.width.grid(row=0, column=1)
+        self.width.name = 'width'
+        Label(self.frame,
+              text='Input desire line color in hex format (#c8fd92): ').grid(row=1, column=0, pady=10)
+        color_input = StringVar()
         color_input.set('#000000')
-        color = tk.Entry(pattern_frame, textvariable=color_input)
-        color.grid(row=1, column=1)
-        color.name = 'color'
+        self.color = Entry(self.frame, textvariable=color_input)
+        self.color.grid(row=1, column=1)
+        self.color.name = 'color'
 
-        tk.Label(pattern_frame,
-                 text='Type your text here...').grid(row=2, column=0)
-        text_box = tk.Entry(pattern_frame, state='normal')
-        text_box.grid(row=2, column=1)
-        text_box.name = 'text'
+        Label(self.frame,
+              text='Type your text here...').grid(row=2, column=0, pady=10)
+        self.text_box = Entry(self.frame, state='normal')
+        self.text_box.grid(row=2, column=1)
+        self.text_box.name = 'text'
+        Label(self.frame, pady=20).grid(row=3)
+        self.generate_button = Button(self.frame, text='Generate pattern',
+                                      padx=10, pady=10)
+        self.generate_button.grid(row=4, column=0)
+        self.edit_button = Button(self.frame, text='Edit background settings',
+                                  pady=10, padx=10)
+        self.edit_button.grid(row=4, column=1)
 
-        tk.Button(pattern_frame, text='Generate pattern', command=lambda:
-                  self.create_pattern(width.get(), color.get(), text_box.get())).grid(row=3, column=0)
-        return pattern_frame
 
-    def generate_custom_settings_frame(self) -> tk.Frame:
-        custom_settings_frame = tk.Frame(self)
-        custom_settings_frame.grid(row=2, column=0)
+class PatternImage:
+    def __init__(self, master):
+        self.frame = Frame(master, width=390, height=370)
+        self.frame.grid(row=1, column=1, pady=5)
 
-        tk.Label(custom_settings_frame,
-                 text='How width (in pixsels) you want the image be? Max(5616)? ').grid(row=0, column=0)
-        width_input = tk.IntVar()
-        width_input.set(4000)
-        width = tk.Entry(custom_settings_frame, textvariable=width_input)
-        width.grid(row=0, column=1)
 
-        tk.Label(custom_settings_frame,
-                 text='How many letters in row you want?: ').grid(row=1, column=0)
-        num_of_columns_input = tk.IntVar()
-        num_of_columns_input.set(7)
-        num_of_columns = tk.Entry(
-            custom_settings_frame, textvariable=num_of_columns_input)
-        num_of_columns.grid(row=1, column=1)
+class PatternGenerator:
+    def __init__(self, master):
+        self.master = master
+        master.title("Pattern Generator")
+        master.geometry('1000x800')
+        master.maxsize(1000, 800)
 
-        tk.Label(custom_settings_frame,
-                 text='Do you want to genreate background mesh?:(yes/no) ').grid(row=2, column=0)
-        with_mesh_input = tk.StringVar()
-        with_mesh_input.set('no')
-        with_mesh = tk.Entry(custom_settings_frame,
-                             textvariable=with_mesh_input)
-        with_mesh.grid(row=2, column=1)
+        self.bg_settings = BackgroundSettings(master)
+        self.bg_image = BackgroundImage(master)
 
-        tk.Label(custom_settings_frame,
-                 text='Input desire background color in hex format (#c8fd92). ').grid(row=3, column=0)
-        color_input = tk.StringVar()
-        color_input.set('#FFFFFF')
-        color = tk.Entry(custom_settings_frame, textvariable=color_input)
-        color.grid(row=3, column=1)
-        tk.Label(custom_settings_frame,
-                 text='Select schema ').grid(row=4, column=0)
-        schema_input = tk.StringVar()
-        schema_input.set('SimplePolishSchema')
-        schema = tk.OptionMenu(
-            custom_settings_frame, variable=schema_input, value='SimplePolishSchema')
-        schema.grid(row=4, column=1)
+        self.pattern_settings = PatternSettings(master)
+        self.disable_children(self.pattern_settings.frame)
+        self.pattern_image = PatternImage(master)
 
-        tk.Button(custom_settings_frame, text='Apply', command=lambda:
-                  self.create_background(width.get(), num_of_columns.get(), with_mesh.get(), color.get(), schema_input.get())).grid(row=5, column=0)
-        self.edit_button_frame = tk.Frame(custom_settings_frame)
-        self.edit_button_frame.grid(row=5, column=1)
-        tk.Button(self.edit_button_frame, text='Edit', state='disabled',
-                  command=lambda: self.enable_children(custom_settings_frame)).grid()
+        self.bg_settings.apply_button['command'] = \
+            lambda: self.create_background(
+                self.bg_settings.width.get(),
+                self.bg_settings.num_of_columns.get(),
+                self.bg_settings.with_mesh.get(),
+                self.bg_settings.color.get(),
+                self.bg_settings.schema_input.get())
+        self.pattern_settings.edit_button['command'] = \
+            lambda: [
+                self.enable_children(self.bg_settings.frame),
+                self.disable_children(self.pattern_settings.frame),
+                ]
+        self.pattern_settings.generate_button['command'] = \
+            lambda: [
+               self.create_pattern(
+                self.pattern_settings.width.get(),
+                self.pattern_settings.color.get(),
+                self.pattern_settings.text_box.get())
+               ]
 
-        return custom_settings_frame
+    def display_background(self, background: Image) -> None:
+        image = background.resize((390, 370))
+        img = ImageTk.PhotoImage(image=image)
+        label = Label(self.bg_image.frame, image=img)
+        label.image = img
+        label.config(bg='black', borderwidth=1)
+        label.grid()
 
     def create_background(self, width: str, num_of_columns: str, with_mesh: str, color: str, schema: str) -> None:
 
@@ -115,6 +169,7 @@ class App(tk.Frame):
             with_mesh = True
         else:
             with_mesh = False
+
         try:
             self.background = ImageBackground(
                 width=width,
@@ -123,13 +178,14 @@ class App(tk.Frame):
                 num_of_colums=num_of_columns,
                 color=color,
                 )
-            self.error_msg.grid_remove()
-            self.disable_children(self.custom_settings_frame)
-            self.enable_children(self.edit_button_frame)
-            self.custom_pattern_settings_frame.grid()
+            self.display_background(
+                self.background.generate_image_background())
+            self.bg_settings.error_msg.grid_remove()
+            self.disable_children(self.bg_settings.frame)
+            self.enable_children(self.pattern_settings.frame)
         except Exception as e:
-            self.error_msg['text'] = f'{e}, try again'
-            self.error_msg.grid()
+            self.bg_settings.error_msg['text'] = f'{e}, try again'
+            self.bg_settings.error_msg.grid(columnspan=2)
 
     def create_pattern(self, width: str, color: str, text: str) -> None:
         try:
@@ -140,19 +196,23 @@ class App(tk.Frame):
                 color=color,
                 start_line_width=width
             )
-            self.save_the_image()
-            self.error_msg.grid_remove()
-            self.succes_msg['text'] = 'Pattern generated, you can find it in the project folder'
-            self.succes_msg.grid()
+            self.display_image(self.pattern.draw())
+            #self.save_the_image()
+            self.pattern_settings.msg.grid_remove()
+            self.pattern_settings.msg['text'] = 'Pattern generated, you can find it in the project folder'
+            self.pattern_settings.msg.grid()
         except Exception as e:
-            self.succes_msg.grid_remove()
-            self.error_msg['text'] = f'{e}'
-            self.error_msg.grid()
+            self.pattern_settings.msg.grid_remove()
+            self.pattern_settings.msg['text'] = f'{e}'
+            self.pattern_settings.msg.grid()
 
-    def save_the_image(self) -> None:
-        image = self.pattern.draw()
-        path = str(os.path.expanduser("~/Desktop/")) + 'pattern.jpg'
-        image.save(path, quality=100)
+    def display_image(self, background: Image) -> None:
+        image = background.resize((390, 370))
+        img = ImageTk.PhotoImage(image=image)
+        label = Label(self.pattern_image.frame, image=img)
+        label.image = img
+        label.config(bg='black', borderwidth=1)
+        label.grid()
 
     def disable_children(self, parent):
         for child in parent.winfo_children():
@@ -171,7 +231,6 @@ class App(tk.Frame):
                 self.enable_children(child)
 
 
-if __name__ == '__main__':
-    root = tk.Tk()
-    myapp = App(root)
-    myapp.mainloop()
+root = Tk()
+my_gui = PatternGenerator(root)
+root.mainloop()
